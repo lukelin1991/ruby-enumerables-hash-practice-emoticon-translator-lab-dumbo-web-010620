4d2e6b3ad5ffbab771ee815eaa68abc270f35a86
Luke Lin <lukelin1991@yahoo.com>
2020-01-06 03:44:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,12 +15,12 @@ def load_library(path)
 end
 
 def get_japanese_emoticon(path, emoticon)
-  emoticon_hash = load_library(path)
-  result = emoticon_hash[:get_emoticon][emoticon]
-  if result == nil
-    result = "Sorry, that emoticon was not found"
+  emote_h = load_library(path)
+  answer = emote_h[:get_emote][emoticon]
+  if answer == nil
+    answer = "Sorry, that emoticon was not found"
   end
-  return result
+  return answer
   # code goes here
 end
 
